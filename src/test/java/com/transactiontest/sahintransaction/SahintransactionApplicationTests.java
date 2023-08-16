1639c75b409a36f283096df361182f09458c6c74
sahinci <sahinci@gmail.com>
2023-08-16 20:36:50 +0300

    db related unit test closed

--- src/test/java/com/transactiontest/sahintransaction/SahintransactionApplicationTests.java
+++ src/test/java/com/transactiontest/sahintransaction/SahintransactionApplicationTests.java
@@ -3,7 +3,7 @@ package com.transactiontest.sahintransaction;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SahintransactionApplicationTests {
 
 	@Test
